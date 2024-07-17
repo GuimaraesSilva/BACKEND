36a4c821dd710a0a3b37f71e87b03d24a51195ba
Fábio Silva <fabio8silva@hotmail.com>
2024-07-17 21:13:00 +0100

    '...'

--- TYPESCRIPT/build/main.js
+++ TYPESCRIPT/build/main.js
@@ -1,3 +1,2 @@
-const x = 123;
-console.log(x);
+console.log("ok!");
 export {};
--- TYPESCRIPT/src/main.ts
+++ TYPESCRIPT/src/main.ts
@@ -1,3 +1,31 @@
-const x: number = 123;
+import express, { Express, Request, Response} from 'express';
 
-console.log(x);
+const app: Express = express();
+
+app.use(express.json());
+
+interface IUser {
+    id: number;
+    name: string;
+    email: string;
+}
+
+let users = [
+    {id: 1,
+    name: 'Jaquim',
+    email: 'jaquimcouves@gmail.com'},
+
+    {id: 2,
+    name: 'Josefina',
+    email: 'finagrega@gmail.com'}
+]
+
+app.get('/users', (req: Request, res:Response) => {
+    res.json(users)
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
