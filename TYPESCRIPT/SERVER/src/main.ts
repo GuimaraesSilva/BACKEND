23ebe9c3f24cdf5c454c760df1d23d5ca9b86a59
Fábio Silva <fabio8silva@hotmail.com>
2024-07-26 23:04:21 +0100

    'user router, controller, service'

--- TYPESCRIPT/SERVER/src/main.ts
+++ TYPESCRIPT/SERVER/src/main.ts
@@ -1,10 +1,12 @@
 import express, {Express} from 'express';
+import cors from 'cors'; 
 import userRouter from './routers/userRouter.js';
 import productsRouter from './routers/product.Router.js';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(productsRouter);
 
--- TYPESCRIPT/SERVER/src/routers/userRouter.ts
+++ TYPESCRIPT/SERVER/src/routers/userRouter.ts
@@ -1,92 +1,21 @@
 import { Router } from 'express';
-import usersControllers from '../controllers/userControllers.js';
+import UserController from '../controllers/userController.js';
 
 const router: Router = Router();
 
 // Get all users
-router.get('/users', usersControllers.getAll);
-// Get user by id
-router.get('/users/:id', usersControllers.getOne)
+router.get('/users', UserController.getAll);
+
+// Get user by ID
+router.get('/users/:id', UserController.getOne);
+
 // Create a new user
-// router.post('/users/', usersControllers.create)
-// // Update an existing user 
-// router.put('/users/:id', usersControllers.update)
-// // Delete an existing 
-// router.delete('/users/:id', usersControllers.delete)
+router.post('/users', UserController.register);
+
+// Update an existing user
+router.put('/users/:id', UserController.update);
+
+// Delete an existing user
+router.delete('/users/:id', UserController.delete);
 
 export default router;
-
-// let users: IUser[] = JSON.parse(readFileSync('./src/data/users.json', 'utf-8'));
-
-// // Get all users
-// router.get('/users', (req: Request, res: Response) => {
-//     console.log('Request Method', req.method);
-//     console.log('Request URL', req.originalUrl);
-//     console.log('Query Parameters', req.query);
-
-//     if (users.length === 0) {
-//         res.statusMessage = 'No users found';
-//         res.sendStatus(204);
-//     } else {
-//         res.json(users);
-//     }
-// });
-
-// // Get user by id
-// router.get('/users/:id', (req: Request, res: Response) => {
-//     const userId: number = parseInt(req.params.id);
-//     const foundUser = users.find(user => user.id === userId);
-
-//     if (!foundUser){
-//         res.status(404).json({error: 'User not found'});
-//     }
-
-//     res.json(foundUser);
-// });
-
-// // Create a new user
-// router.post('/users/', (req: Request, res: Response) => {
-//     console.log('Request Body', req.body);
-
-//     const newUser: IUser= {
-//         id: users.length + 1,
-//         name: req.body.name,
-//         email: req.body.email
-//     };
-
-//     users.push(newUser);
-//     res.status(201).json(newUser);
-// });
-
-// // Update an existing user 
-// router.put('/users/:id', (req: Request, res: Response) => {
-//     const userId: number = parseInt(req.params.id);
-//     const foundUser: IUser | undefined = users.find(user => user.id === userId);
-
-//     if (!foundUser){
-//         res.status(404).json({error: 'User not found'});
-//     }else {
-//         foundUser.name = req.body.name;
-//         foundUser.email = req.body.email;
-//         foundUser.password = req.body.password;
-//     }
-//     res.json(foundUser);
-// });
-
-// // Delete an existing 
-// router.delete('/users/:id', (req: Request, res: Response) => {
-//     const userId: number = parseInt(req.params.id);
-//     const foundUser: IUser | undefined = users.find(user => user.id === userId);
-
-//     if (!foundUser){
-//         res.status(404).json({error: 'User not found'});
-//     }else {
-//         users = users.filter((user) => user.id !== userId);
-//     }
-//     res.json(foundUser);
-// });
-
-// // Endpoint Search - GET"/search?email=alex@gmail.com"
-
-
-// export default router;
